d533b0bc4e39403272882dfb3c4934a20413b09a
Mr. Karin Ramirez <kdramir1@uno.edu>
2026-03-24 23:44:19 -0500

    Set up initial server entry point and startup flow

---
+++ src/main/java/com/matchinggame/server/MatchingGameServer.java
@@ -0,0 +1,36 @@
+package com.matchinggame.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+//represents the multiplayer server for the Matching Game.
+//stores the port and confirms startup
+
+public class MatchingGameServer {
+
+	private final int serverPort;
+	private ServerSocket serverSocket;
+
+	//constructor
+	public MatchingGameServer(int serverPort){
+		this.serverPort = serverPort;
+	}
+
+	//start the server and listens for incoming client connection	
+
+	public void startServer(){
+		try{
+			serverSocket = new ServerSocket(serverPort);
+			System.out.println("Server is listening on port: " + serverPort);
+
+			while(true){
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("New client connected from " + clientSocket.getInetAddress());
+			}
+		}
+		catch (IOException ioException){
+			System.out.println("Server Error: " + ioException.getMessage());
+		}
+	}
+}
--- src/main/java/com/matchinggame/server/ServerMain.java
+++ src/main/java/com/matchinggame/server/ServerMain.java
@@ -1,4 +1,33 @@
 package com.matchinggame.server;
 
+
+ //entry point for the matching game server application.
+ // this class is responsible only for starting the server.
+ 
+
 public class ServerMain {
-}
+
+	private static final int DEFAULT_PORT = 8989;
+
+	public static void main(String [] args){
+
+		int serverPort = DEFAULT_PORT;
+
+		if(args.length >= 1){
+			try{
+				serverPort = Integer.parseInt(args[0]);
+			}
+			catch (NumberFormatException NumberFormatException){
+				System.out.println("Invalid port provided. Using default port 8989.");
+			}
+		}
+		else{
+			System.out.println("No port provided. using default port 8989.");
+		}
+
+		MatchingGameServer matchingGameServer = new MatchingGameServer(serverPort);
+		matchingGameServer.startServer();
+
+	}//end of main
+
+}//end of class
